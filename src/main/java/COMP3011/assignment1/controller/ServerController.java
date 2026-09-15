923cf0ea1d324236b88ae9f62d8afc21a4246f47
Akhay-006 <akhay006@mymail.unisa.edu.au>
2026-09-15 17:13:51 +0930

    Modified serverController and added server uptime endpoint

--- src/main/java/COMP3011/assignment1/controller/ServerController.java
+++ src/main/java/COMP3011/assignment1/controller/ServerController.java
@@ -3,6 +3,13 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import COMP3011.assignment1.service.TokenTrackerService;
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class ServerController {
     private final TokenTrackerService tokenTrackerService;
@@ -17,6 +24,37 @@ public class ServerController {
 
         this.tokenTrackerService = tokenTrackerService;
         this.context = context;
+    }
+    @GetMapping(
+            value = "/api/v1/admin/uptime",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Map<String, Object>> getUptime() {
+
+        Instant now = Instant.now();
+
+        Map<String, Object> response =
+                new LinkedHashMap<>();
+
+        response.put(
+                "utcServerStart",
+                tokenTrackerService
+                        .getServerStart()
+                        .toString()
+        );
+
+        response.put(
+                "utcNow",
+                now.toString()
+        );
+
+        response.put(
+                "serverUptimeSeconds",
+                tokenTrackerService
+                        .getUptimeSeconds()
+        );
+
+        return ResponseEntity.ok(response);
     }
 
 }
